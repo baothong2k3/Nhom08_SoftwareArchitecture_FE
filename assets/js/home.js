aca3a722237d44cf3b20b19097ef53d5bb464c01
ANHHAI <haianhchidan@gmail.com>
2025-03-07 14:58:23 +0700

    Home

---
+++ assets/js/home.js
@@ -0,0 +1,32 @@
+document.addEventListener("keydown", function (event) {
+  if (
+    event.ctrlKey &&
+    (event.key === "=" || event.key === "-" || event.key === "0")
+  ) {
+    event.preventDefault();
+  }
+});
+
+document.addEventListener(
+  "wheel",
+  function (event) {
+    if (event.ctrlKey) {
+      event.preventDefault();
+    }
+  },
+  { passive: false }
+);
+
+fetch("../components/footer.html")
+  .then((response) => response.text())
+  .then((data) => {
+    document.getElementById("footer").innerHTML = data;
+  })
+  .catch((error) => console.error("Error loading footer:", error));
+
+fetch("../components/menu.html")
+  .then((response) => response.text())
+  .then((data) => {
+    document.getElementById("menu").innerHTML = data;
+  })
+  .catch((error) => console.error("Error loading footer:", error));
